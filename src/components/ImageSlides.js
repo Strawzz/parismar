f3c1cb6599634226c6868cbc5e4089f289f5e432
Zhuo <zhuo.parse@gmail.com>
2023-08-12 21:47:33 -0700

    Completed slide show

--- src/components/ImageSlides.js
+++ src/components/ImageSlides.js
@@ -7,59 +7,65 @@ import show2 from '../styles/show2.jpeg';
 import show3 from '../styles/show3.jpeg';
 import show4 from '../styles/show4.jpeg';
 
-
 const spanStyle = {
     padding: '20px',
     background: '#efefef',
-    color: '#000000'
+    color: '#000000',
+    position: 'absolute', // Position the caption absolutely within the slide's div
+    bottom: '0', // Place the caption at the bottom of the div
+    width: '100%', // Make the caption span the full width of the div
+    boxSizing: 'border-box', // Include padding in the full width
+}
+
+const containerStyle = {
+    position: 'relative', // Position the container to handle child positioning
+    maxWidth: '800px', // Adjust the container width as needed
+    margin: '0 auto', // Center the container horizontally
 }
 
 const divStyle = {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundSize: 'contain',
-    height: '400px',
-    maxWidth: '100%', 
-    maxHeight: '100%', 
-
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    height: '600px',
+    width: '100%',
+    position: 'relative', // Ensure relative positioning for the caption
 }
+
 const slideImages = [
     {
-        url: aboutImage,
+        url: show1,
         caption: 'Slide 1'
     },
     {
-        url:show1 ,
+        url: show2,
         caption: 'Slide 2'
     },
     {
-        url:show2 ,
-        caption: 'Slide 2'
+        url: show3,
+        caption: 'Slide 3'
     },
     {
-        url:show3 ,
-        caption: 'Slide 2'
-    },
-    {
-        url:show4 ,
-        caption: 'Slide 2'
+        url: show4,
+        caption: 'Slide 4'
     },
 ];
 
 const Slideshow = () => {
     return (
-        <div className="slide-container">
+        <div className="slide-container" style={containerStyle}>
             <Slide duration={1000}>
-            {slideImages.map((slideImage, index)=> (
-                <div key={index}>
-                <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
-                    <span style={spanStyle}>{slideImage.caption}</span>
-                </div>
-                </div>
-            ))} 
+                {slideImages.map((slideImage, index) => (
+                    <div key={index} style={{ ...divStyle, backgroundImage: `url(${slideImage.url})` }}>
+                        <span style={spanStyle}>{slideImage.caption}</span>
+                    </div>
+                ))}
             </Slide>
         </div>
     )
 }
+
 export default Slideshow;
